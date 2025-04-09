0296184791737590d0653e506b30a82c60112b4e
leinerdev <leinerbarrios99.ljbm@gmail.com>
2025-04-09 18:41:02 -0500

    ci: 🎡 new message to hello controller

--- src/helloworld/hello.controller.ts
+++ src/helloworld/hello.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class HelloController {
   @Get()
   public hello(): string {
-    return 'Hola Mundo desde API de NestJS';
+    return 'Hola Mundo desde API de NestJS con GraphQL';
   }
 }
